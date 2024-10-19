7fdb1240077864839c1828251bf8ce5045c82c44
sujay-707 <sujaymundaragi7@gmail.com>
2024-10-19 15:17:36 +0530

    added root route check

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ let port = 3000;
 let cors = require('cors');
 app.use(cors());
 
+app.get("/" , (req , res) =>
+{
+  res.send("Ok Fine Check Done");
+})
+
 app.listen(port, () => {
   console.log('App is Listening to port ' + port);
 });
